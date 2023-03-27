ada5e614469ee084cbdcee5f45ec943a497c03ac
el tintus <martin.hiriart03@gmail.com>
2023-03-27 15:25:51 -0300

    proyecto turdera primera subida

---
+++ turdera.js
@@ -0,0 +1,8 @@
+
+
+function showdata(value, id, prop, prop2){
+    document.getElementById("demo").innerHTML = value;
+    document.getElementById(id).style.color = prop;
+    document.getElementById(id).style.textDecorationColor = prop2;
+}
+
